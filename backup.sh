171afb97716750a9fa005e99d06a792835c48443
rojaq <jacko4765@gmail.com>
2023-03-26 23:40:44 +0200

    Added a new backup file and modified the old one

--- backup.sh
+++ backup.sh
@@ -1,55 +1,16 @@
-﻿#!/usr/bin/env bash
-BACKUP=${HOME}/backup
-function backup(){
-	while true
-	do
-		if [[ ! -e $BACKUP ]];then
-			mkdir $BACKUP
-		fi
-		echo
-		echo -e "Hello to the backup program. Plese choose one of the below options:\n1. Backup\n2. Delete backup\n3. Show backed up files\n4. Change backup folder\nQ to quit: "
-		read OPTION
-		case $OPTION in
-			1|[Bb]ackup)
-				echo -e "FILES:\n"
-				ls -1
-				read -p "Please choose a file/files: " FILES
-				if [ -e $FILES ];then
-					if [ -f $FILES ];then
-						cp $FILES $BACKUP
-					elif [ -d $FILES ];then
-						cp -r $FILES $BACKUP
-					fi
-					echo "Backup successfull"
-				else
-					echo "Unsuccessfull -doesn't exist"
-				fi
-				;;
-			2|[Dd]elete)
-				echo -e "Choose a file to delete:\n"
-				ls -1 $BACKUP
-				read FILES
-				if [ -e $BACKUP/$FILES ];then
-					rm $BACKUP/$FILES
-					echo "Deleted"
-				else
-					echo "File doesn't exist"
-				fi
-				;;
-			3|[Ss]how)
-				ls -1 $BACKUP
-				;;
-			4)
-				read -p "Please type in a new folders name: " BACKUP
-				echo "Changed new backup folder path to: $BACKUP"
-				;;
-			"q"|"Q")
-				return 0
-				;;
-			*)
-				echo "Please try again"
-				;;
-		esac
-	done
-}
-backup
+#!/bin/bash
+# NAME: Jacek
+# DATE: 03/26/2023 11:23:03 PM CEST
+# DESCRIPTION: backup /var and /etc directories
+
+tar cvf /tmp/backup.tar /etc /var
+gzip /tmp/backup.tar
+find /tmp/backup.tar.gz -mtime -1 -type f -print &> /dev/null
+if [[ $? == 0 ]]; then
+	echo "Backup created"
+	echo
+	echo "Archiving backup"
+	# scp /tmp/backup.tar.gz root@192...:/path
+else
+	echo "Did not create the backup"
+fi
---
+++ personal-backup.sh
@@ -0,0 +1,55 @@
+﻿#!/usr/bin/env bash
+BACKUP=${HOME}/backup
+function backup(){
+	while true
+	do
+		if [[ ! -e $BACKUP ]];then
+			mkdir $BACKUP
+		fi
+		echo
+		echo -e "Hello to the backup program. Plese choose one of the below options:\n1. Backup\n2. Delete backup\n3. Show backed up files\n4. Change backup folder\nQ to quit: "
+		read OPTION
+		case $OPTION in
+			1|[Bb]ackup)
+				echo -e "FILES:\n"
+				ls -1
+				read -p "Please choose a file/files: " FILES
+				if [ -e $FILES ];then
+					if [ -f $FILES ];then
+						cp $FILES $BACKUP
+					elif [ -d $FILES ];then
+						cp -r $FILES $BACKUP
+					fi
+					echo "Backup successfull"
+				else
+					echo "Unsuccessfull -doesn't exist"
+				fi
+				;;
+			2|[Dd]elete)
+				echo -e "Choose a file to delete:\n"
+				ls -1 $BACKUP
+				read FILES
+				if [ -e $BACKUP/$FILES ];then
+					rm $BACKUP/$FILES
+					echo "Deleted"
+				else
+					echo "File doesn't exist"
+				fi
+				;;
+			3|[Ss]how)
+				ls -1 $BACKUP
+				;;
+			4)
+				read -p "Please type in a new folders name: " BACKUP
+				echo "Changed new backup folder path to: $BACKUP"
+				;;
+			"q"|"Q")
+				return 0
+				;;
+			*)
+				echo "Please try again"
+				;;
+		esac
+	done
+}
+backup
